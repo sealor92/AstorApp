7e1108a812c0f080742105cbb7e2c4c21b05ff51
sealor92 <sebasalcon9214@hotmail.com>
2015-06-11 20:09:00 -0500

    practica_6

--- app/src/main/java/sebastianalarcon/com/astorapp/Splash.java
+++ app/src/main/java/sebastianalarcon/com/astorapp/Splash.java
@@ -1,109 +1,53 @@
 package sebastianalarcon.com.astorapp;
-
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Splash.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Splash#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Splash extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Splash.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Splash newInstance(String param1, String param2) {
-        Splash fragment = new Splash();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public Splash() {
-        // Required empty public constructor
-    }
-
+public class Splash extends Activity {
+    private static final long SPLASH_DELAY = 3000;
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent().setClass(Splash.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task,SPLASH_DELAY);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
+        return true;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.splash, container, false);
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
 }
--- app/src/main/java/sebastianalarcon/com/astorapp/formulario.java
+++ app/src/main/java/sebastianalarcon/com/astorapp/formulario.java
@@ -1,19 +1,59 @@
 package sebastianalarcon.com.astorapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.CursorIndexOutOfBoundsException;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class formulario extends ActionBarActivity {
+public class formulario extends ActionBarActivity implements View.OnClickListener {
+
+    DataBaseManager Manager = MainActivity.getManager();
+    private Cursor cursor;
+    private ListView lista;
+    private SimpleCursorAdapter adapter;
+    private EditText Ednombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        lista = (ListView) findViewById(android.R.id.list);
+        Ednombre = (EditText) findViewById(R.id.EdText1);
+
+        String[] from = new String[]{Manager.CN_ID,Manager.CN_NAME};
+        int[] to = new int[]{android.R.id.text1,android.R.id.text2};
+        cursor = Manager.cargarCursorContactos();
+        adapter = new SimpleCursorAdapter(this,android.R.layout.two_line_list_item,cursor,from,to,0);
+        lista.setAdapter(adapter);
+
+        //Manager.insertar("Alejo","5822128");
+        //Manager.insertar("Pablo","2651752");
+        //Manager.insertar("Paula","4910413");
+        Button btnbuscar = (Button) findViewById(R.id.btn1);
+        btnbuscar.setOnClickListener(this);
+        Button btncargar = (Button) findViewById(R.id.btndb);
+        btncargar.setOnClickListener(this);
+        Button btninsertar = (Button) findViewById(R.id.btninsertar);
+        btninsertar.setOnClickListener(this);
+        Button btneliminar = (Button) findViewById(R.id.btneliminar);
+        btneliminar.setOnClickListener(this);
+        Button btnactualizar = (Button) findViewById(R.id.btnactualizar);
+        btnactualizar.setOnClickListener(this);
     }
+
 
 
     @Override
@@ -31,18 +71,106 @@ public class formulario extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.main) {
-            Intent m = new Intent(this,MainActivity.class);
-            startActivity(m);
-            return true;
-        }
-
         if (id == R.id.mapa) {
             Intent m = new Intent(this,mapa.class);
             startActivity(m);
             return true;
         }
+        if (id == R.id.main) {
+            Intent ma = new Intent(this,MainActivity.class);
+            startActivity(ma);
+            return true;
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==R.id.btn1){
+            new BuscarTask().execute();
+        }
+        if(v.getId()==R.id.btndb){
+            lista = (ListView) findViewById(android.R.id.list);
+            Ednombre = (EditText) findViewById(R.id.EdText1);
+
+            String[] from = new String[]{Manager.CN_NAME,Manager.CN_LAT};
+            int[] to = new int[]{android.R.id.text1,android.R.id.text2};
+            cursor = Manager.cargarCursorContactos();
+            adapter = new SimpleCursorAdapter(this,android.R.layout.two_line_list_item,cursor,from,to,0);
+            lista.setAdapter(adapter);
+
+        }
+        if (v.getId()==R.id.btninsertar){
+            EditText nombre = (EditText) findViewById(R.id.EdNombre);
+            EditText latitud = (EditText) findViewById(R.id.EdLatitud);
+            EditText longitud = (EditText) findViewById(R.id.EdLong);
+            Manager.insertar(nombre.getText().toString(),latitud.getText().toString(),longitud.getText().toString());
+            nombre.setText("");
+            latitud.setText("");
+            longitud.setText("");
+            Toast.makeText(getApplicationContext(), "Insertado", Toast.LENGTH_SHORT).show();
+        }
+        if(v.getId()==R.id.btneliminar){
+            EditText nombre = (EditText) findViewById(R.id.EdNombre);
+            Manager.eliminar(nombre.getText().toString());
+            Toast.makeText(getApplicationContext(),"Eliminado", Toast.LENGTH_SHORT).show();
+            nombre.setText("");
+        }
+        if (v.getId()==R.id.btnactualizar){
+            EditText nombre = (EditText) findViewById(R.id.EdNombre);
+            EditText latitud = (EditText) findViewById(R.id.EdLatitud);
+            EditText longitud = (EditText) findViewById(R.id.EdLong);
+            Manager.Modificar(nombre.getText().toString(),latitud.getText().toString(),longitud.getText().toString());
+            Toast.makeText(getApplicationContext(),"Actualizado", Toast.LENGTH_SHORT).show();
+            nombre.setText("");
+            latitud.setText("");
+            longitud.setText("");
+        }
+    }
+
+
+    private class BuscarTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(),"Buscando...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            cursor = Manager.buscarContacto(Ednombre.getText().toString());
+            return null;
+        }
+
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(getApplicationContext(),"Finalizado", Toast.LENGTH_SHORT).show();
+            adapter.changeCursor(cursor);
+            obtener();
+        }
+
+
+    }
+
+    public void obtener () {
+        TextView Txnombre = (TextView) findViewById(R.id.Txnombre);
+        TextView Txlatitud = (TextView) findViewById(R.id.TxLatitud);
+        TextView Txlongitud = (TextView) findViewById(R.id.TxLongitud);
+        if (cursor.moveToFirst()){
+            String dbnombre = cursor.getString(cursor.getColumnIndex(Manager.CN_NAME));
+            Txnombre.setText(dbnombre);
+            String dblatitud = cursor.getString(cursor.getColumnIndex(Manager.CN_LAT));
+            Txlatitud.setText(dblatitud);
+            String dblongitud = cursor.getString(cursor.getColumnIndex(Manager.CN_LONG));
+            Txlongitud.setText(dblongitud);}
+        else{
+            Txnombre.setText("No Found");
+            Txlatitud.setText("No Found");
+            Txlongitud.setText("No Found");
+        }
+
     }
 }
